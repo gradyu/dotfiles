32bdd92e200608e35a4da6d9699442a490e95776
Grady <yhwei876@gmail.com>
2018-12-06 20:25:25 +0800

    add alias.

--- shell/ohmyzsh/my_configs.zsh
+++ shell/ohmyzsh/my_configs.zsh
@@ -12,8 +12,13 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=15'
 #aliases
 alias cls=clear
 alias rmd="rm -rf"
+
+alias npmgin="npm i -g"
+alias npmgrm="npm u -g"
 alias npmgls="npm list -g --depth 0"
 alias npmclean="npm cache clean --force"
+
 alias pipls="pip list"
 alias pipout="pip list --outdated"
+
 alias eczsh="ec ~/.zshrc"
