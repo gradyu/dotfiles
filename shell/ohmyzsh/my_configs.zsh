dcca4a579dd7871d4c5497e41904540fc1ec3070
Grady <yhwei876@gmail.com>
2019-08-08 19:58:34 +0800

    add homebrew aliases.

--- shell/ohmyzsh/my_configs.zsh
+++ shell/ohmyzsh/my_configs.zsh
@@ -11,23 +11,37 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=15'
 
 #command -v minikube > /dev/null 2>&1 && minikube status > /dev/null 2>&1 && eval $(minikube docker-env)
 
-#aliases
+# aliases
 alias cls=clear
 alias rmd="rm -rf"
 
+# npm
 alias npmgin="npm i -g"
 alias npmgrm="npm u -g"
 alias npmgls="npm list -g --depth 0"
 alias npmclean="npm cache clean --force"
 
+#pip
 alias pipls="pip list"
 alias pipin= "pip install --user"
 alias piprm="pip uninstall --user"
 alias pipout="pip list --outdated"
 
+# kubectl
 alias kapod="kubectl get pod --all-namespaces -o wide"
 alias kasvc="kubectl get svc --all-namespaces -o wide"
 alias kpod="kubectl get pod"
 alias ksvc="kubectl get svc"
+
+# homebrew
+alias cask="brew cask"
+alias bins="brew install"
+alias brm="brew uninstall"
+alias bcs="brew cask list"
+alias bcinfo="brew cask info"
+alias bcins="brew cask install"
+alias bcrm="brew cask uninstall"
+alias bcrins="brew cask reinstall"
+alias bcupg="brew cask upgrade"
 
 alias eczsh="ec ~/.zshrc"
